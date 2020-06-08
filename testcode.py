4593e61a3e22e51ec02c8a0af16dccfa9630cee9
Matthias Faust <faust@tropos.de>
2020-06-08 10:26:44 +0200

    first code lines

---
+++ testcode.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+dt = 0.01
+t = np.arange(0, 30, dt)
+nse1 = np.random.randn(len(t))                 # white noise 1
+nse2 = np.random.randn(len(t))                 # white noise 2
